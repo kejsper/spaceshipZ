45aa42a33e9ea831a3131cf117299e5c794f8e35
Kacper Tylenda <k.tylenda@x-coding.pl>
2018-03-25 13:24:51 +0200

    start date datepicker play

--- src/components/common/forms/StartDate.vue
+++ src/components/common/forms/StartDate.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="hello">
-
+    <datepicker placeholder="Pick up date" v-model="startPicked" @input="setStartDate()"></datepicker>
+    {{startPicked}}
   </div>
 </template>
 
 <script>
+import Datepicker from 'vuejs-datepicker'
+
 export default {
-  name: 'PickUp'
+  name: 'PickUp',
+  data () {
+    return {
+      startPicked: null
+    }
+  },
+  components: {
+    Datepicker
+  },
+  methods: {
+    setStartDate () {
+      console.log(this.startPicked)
+    }
+  }
 }
 </script>
 
