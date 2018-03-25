e7e708ca6e04cc7e7c0695570b47e972f046f9a8
kejsper <kejsper@gmail.com>
2018-03-25 03:40:34 +0200

    added search button

--- src/components/common/Filters.vue
+++ src/components/common/Filters.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="hello">
-
+  <div class="filters">
+    <search-button @click.native="goToList"/>
   </div>
 </template>
 
 <script>
+import SearchButton from './forms/SearchButton'
+
 export default {
-  name: 'Filters'
+  name: 'Filters',
+  components: {
+    SearchButton
+  },
+  methods: {
+    goToList () {
+      console.log('kliknięte')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../../assets/scss/variables';
 @import '../../assets/scss/mixins';
+
+.filters {
+  @include flex (column, start, start);
+  width: 100%;
+}
 
 </style>
