8ed4e6eb8bd75f8295fe8e1db22965bf35e08754
kejsper <kejsper@gmail.com>
2018-03-28 03:34:11 +0200

    cleaning spaceship list route component

---
+++ src/components/common/spaceshipList/SearchCriteria.vue
@@ -0,0 +1,64 @@
+<template>
+  <div classs="spaceships__wrapper">
+    <h2 class="spaceships__title">Search criteria</h2>
+    <p>
+      Flying from {{ activeFilters.pickup }} ({{ activeFilters.startDate }}).
+      Flying to {{ activeFilters.dropoff }} ({{activeFilters.endDate}}).
+    </p>
+    <button @click.prevent="expandAdvanced = !expandAdvanced" class="spaceships__button">Advanced filters</button>
+    <advanced-filters v-if="expandAdvanced" :activeFilters="activeFilters"/>
+  </div>
+</template>
+
+<script>
+import AdvancedFilters from '../forms/AdvancedFilters'
+
+export default {
+  name: 'SearchCriteria',
+  props: ['activeFilters'],
+  components: {
+    AdvancedFilters
+  },
+  data () {
+    return {
+      expandAdvanced: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/scss/variables';
+@import '../../../assets/scss/mixins';
+.spaceships {
+  @include flex(column, center, center);
+  width: 100%;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  &__title {
+    font-family: $font-normal;
+    font-size: 1.1em;
+    color: $color-violet;
+    font-weight: bold;
+  }
+  &__wrapper{
+    @include flex(column, flex-start, flex-start);
+    width: 100%;
+  }
+  &__button {
+    align-self: center;
+    margin: 1em 0;
+    background-color: transparent;
+    color: $color-violet;
+    font-weight: 500;
+    font-size: 0.75em;
+    border: none;
+    outline: none;
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
--- src/components/common/spaceshipList/SpaceshipsItems.vue
+++ src/components/common/spaceshipList/SpaceshipsItems.vue
@@ -1,7 +1,10 @@
 <template>
-  <ul class="spaceship-list">
-    <spaceships-item v-for="(spaceship, key) in availableSpaceships" :key="key" :spaceship="spaceship" :activeFilters="activeFilters"/>
-  </ul>
+  <section class="spaceships">
+    <h2 class="spaceships__title">Search results ({{availableSpaceships.length}})</h2>
+    <ul class="spaceship-list">
+      <spaceships-item v-for="(spaceship, key) in availableSpaceships" :key="key" :spaceship="spaceship" :activeFilters="activeFilters"/>
+    </ul>
+  </section>
 </template>
 
 <script>
@@ -18,6 +21,19 @@ export default {
 <style lang="scss" scoped>
 @import '../../../assets/scss/variables';
 @import '../../../assets/scss/mixins';
+
+.spaceships {
+  @include flex(column, flex-start, flex-start);
+  width: 100%;
+  max-width: 650px;
+
+  &__title {
+    font-family: $font-normal;
+    font-size: 1.1em;
+    color: $color-violet;
+    font-weight: bold;
+  }
+}
 .spaceship-list {
   @include flex(column, flex-start, flex-start);
   width: 100%;
